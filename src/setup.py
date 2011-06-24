a1f3ae8e53aaecbf020e040d2c77e88d4c768211
Mike English <mike.english@gmail.com>
2011-06-24 17:24:37 -0400

    @WIP Refactor for packaging.

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyGephi',
+    version='0.0.1a',
+    description='Python interface to the Gephi Graph Streaming API',
+    url='https://github.com/panisson/pygephi_graphstreaming',
+    packages=['pygephi'],
+)
+
